4b15ff525e5a7893d75eee4fa6de68e2ad97a878
Peter Ke <peterke0911@gmail.com>
2024-11-05 20:20:04 -0800

    more plugins

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -332,4 +332,25 @@ return {
   },
   -- Schema support for JSON/YAML
   { 'b0o/schemastore.nvim' },
+  -- LSP symbols window
+  {
+    'stevearc/aerial.nvim',
+    opts = {
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+      end,
+    },
+    config = function(_, opts)
+      require('aerial').setup(opts)
+
+      vim.keymap.set(
+        'n',
+        '<leader>a',
+        '<cmd>AerialToggle<CR>',
+        { desc = 'Toggle Aerial' }
+      )
+    end,
+  },
 }
